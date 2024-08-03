7191a96d50b0954c1452a5f6f5973a111e3fdffd
danny1998cuba <danny.glezcuet98@gmail.com>
2024-08-03 07:55:25 -0400

    Initial setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ProvidersListComponent } from './providers-list/providers-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LoginPageComponent
+    },
+    {
+        path: 'providers',
+        component: ProvidersListComponent
+    },
+    {
+        path: '**',
+        redirectTo: '/providers',
+        pathMatch: 'full'
+    },
+];
---
+++ src/app/providers-list/providers-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-providers-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './providers-list.component.html',
+  styleUrl: './providers-list.component.scss'
+})
+export class ProvidersListComponent {
+
+}

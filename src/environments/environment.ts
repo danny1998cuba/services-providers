5785efa7bf53088b5116c277cf21f5f58b3caa56
Daniel González Cuétara <48300769+danny1998cuba@users.noreply.github.com>
2025-03-15 11:11:20 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: true,
     firebaseConfig: {
-        apiKey: "AIzaSyAIyR_dariYpIaZD8z88I-88CUi5IcQdEI",
-        authDomain: "directorio-malaga-b6c73.firebaseapp.com",
-        projectId: "directorio-malaga-b6c73",
-        storageBucket: "directorio-malaga-b6c73.appspot.com",
-        messagingSenderId: "710023200923",
-        appId: "1:710023200923:web:ad4431963d9e25035d463e",
-        measurementId: "G-BCDZX8EX98"
+        apiKey: "AIzaSyC7Ijd9_uDNxx7TSy4R1C-CH6PTgdRgfug",
+  authDomain: "services-providers-9cc54.firebaseapp.com",
+  projectId: "services-providers-9cc54",
+  storageBucket: "services-providers-9cc54.firebasestorage.app",
+  messagingSenderId: "827907307325",
+  appId: "1:827907307325:web:95efb8789da9b7d81c3754",
+  measurementId: "G-WPRJEWKZQP"
     },
     userName: 'authorized_user'
 };
